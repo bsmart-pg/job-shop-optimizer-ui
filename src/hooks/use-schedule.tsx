8b1e523cf68ce4788d75413d2138312c25dce815
Patrick <patrick@dhcp-172-21-60-95.wlan.uni-hamburg.de.local>
2025-04-16 12:45:42 +0200

    fix import error

--- src/hooks/use-schedule.tsx
+++ src/hooks/use-schedule.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Schedule } from "@/lib/types";
-import { api } from "@/lib/api";
+import * as api from "@/lib/api";
 import { useToast } from "@/components/ui/use-toast";
 
 interface UseScheduleReturn {
